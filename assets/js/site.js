cf7fd455c7d552c611e61dee2f2d051b109106ab
Fin Christensen <christensen.fin@gmail.com>
2018-10-24 13:40:21 +0200

    Disable inputs when loading console

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,7 +1,21 @@
 var form = document.getElementById('toolbar');
+var consoleFrame = document.getElementById('console');
+var nameInput = document.getElementById('name');
+var runButton = document.getElementById('run');
+var disabled = false;
+
 form.addEventListener('submit', function(event) {
-    var uri = document.location.origin + '/terminal/' +
-        document.getElementById('name').value;
-    document.getElementById('console').src = uri;
+    if (!disabled) {
+        disabled = true;
+        nameInput.disabled = true;
+        runButton.disabled = true;
+        consoleFrame.src = document.location.origin + '/terminal/' + nameInput.value;
+        consoleFrame.onload = () => {
+            disabled = false;
+            nameInput.disabled = false;
+            runButton.disabled = false;
+        };
+    }
+
     event.preventDefault();
 }, false);
